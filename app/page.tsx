f44a4c5b8f9fad48ef0b41fe7155dd4d396e1a1d
Yuri Bychkov <yuribychkov15@gmail.com>
2025-08-28 16:02:47 -0400

    utility and theme classes added

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen bg-amber-100">
-      <h1 className="font-poppins font-bold text-[var(--color-red)] text-3xl">StoreIt - The only storage you'll need.</h1>
+    <div className="flex-center h-screen">
+      <h1 className="h1">StoreIt - The only storage you'll need.</h1>
     </div>
   );
 }
