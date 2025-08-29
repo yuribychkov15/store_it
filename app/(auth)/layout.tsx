2aff08efd4c2080fbb103e18ad3fc1846439921a
Yuri Bychkov <yuribychkov15@gmail.com>
2025-08-29 17:58:40 -0400

    auth group for sign-in and sign-up started

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Image from 'next/image';
+
+const Layout = ({
+    children
+}: {
+    children: React.ReactNode
+}) => {
+    return (
+        <div className="flex min-h-screen">
+            <section className="bg-brand p-10">
+                <div>
+                    <Image 
+                        src="/favicon.ico"
+                        alt="logo"
+                        width={16}
+                        height={16}
+                        className="h-auto"
+                    />
+                    <div className="space-y-5 text-white">
+                        <h1 className="h1">Manage your files the best way.</h1>
+                        <p className="body-1">A place you can store all your documents</p>
+                    </div>
+                </div>
+            </section>
+            {children}
+        </div>
+    )
+}
+
+export default Layout;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,6 @@
+
+export default function SignUp() {
+    return (
+        <h1>Sign-Up Page</h1>
+    )
+}
