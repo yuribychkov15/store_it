f2e805e29f05f5e5bbfe20a0cc2e292e46b3eee8
Yuri Bychkov <yuribychkov15@gmail.com>
2025-08-30 16:00:29 -0400

    basic form added to sign-in and sign-up

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,6 +1,67 @@
+'use client';
+
+import React from 'react'
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+        message: "Username must be at least 2 characters."
+    })
+})
+
 
 const SignIn = () => {
-    return <div>Sign In</div>
-}   
+
+const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+        username: "",
+    }
+})
+
+function onSubmit(values: z.infer<typeof formSchema>) {
+    // do something when user submits 
+    console.log(values);
+}
+
+  return (
+    <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField 
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl>
+                            <Input placeholder="shadcn" {...field} />
+                        </FormControl>
+                        <FormDescription>
+                            This is your public display name.
+                        </FormDescription>
+                        <FormMessage />
+                    </FormItem>
+  )}
+            />
+            <Button type="submit">Submit</Button>
+        </form>
+    </Form>
+  )
+}
+
 
 export default SignIn
---
+++ components/AuthForm.tsx
@@ -0,0 +1,13 @@
+'use server';
+
+import React from 'react'
+
+type FormType = 'sing-in' | 'sign-up';
+
+const AuthForm = () => {
+  return (
+    <div>AuthForm</div>
+  )
+}
+
+export default AuthForm
